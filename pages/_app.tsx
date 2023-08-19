c14336f1473bfdceb1d1b3c8ef3b66a49e8231ab
amanswarnakar <amanswarnakar2002@gmail.com>
2023-08-19 22:58:26 +0530

    Create Navbar, Login and Register Page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,34 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { Poppins } from "next/font/google";
+import "@/styles/globals.css";
+
+import { SessionProvider } from "next-auth/react";
+import Head from "next/head";
+import type { AppProps } from "next/app";
+
+import Layout from "@/components/Layout";
+
+const poppins = Poppins({
+  weight: ["400", "500", "600", "700", "800"],
+  style: "normal",
+  subsets: ["latin"],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <SessionProvider session={pageProps.session}>
+      <main className={poppins.className}>
+        <Head>
+          <title>News App</title>
+          <meta
+            property="og:title"
+            content="News Web Application"
+            key="title"
+          />
+        </Head>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </main>
+    </SessionProvider>
+  );
 }
